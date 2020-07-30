945056f868cb3fd7ace1f13e437b65aa576532fa
Rachel Friedman <rachelf21@gmail.com>
2020-07-30 19:25:24 -0400

    added states and usa charts

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ from datetime import datetime
 import plotly 
 import plotly.graph_objs as go
 
+selected_state = "California"
 graphJSON_usa_cases="[]"
+graphJSON_usa_deaths="[]"
+graphJSON_states_cases="[]"
+graphJSON_states_deaths="[]"
+
 
 def get_usa_data():
     usa_data = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv", error_bad_lines=False)
@@ -17,19 +22,70 @@ def get_usa_data():
     print("retrieving USA data")
     return usa_data
 
+
+def get_states_data():
+    states_data = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv", error_bad_lines=False)
+    states_data['date'] = pd.to_datetime(states_data['date'], format="%Y-%m-%d" )
+    states_data['date'] = states_data['date'].dt.date
+    st = selected_state
+    states_data = states_data[states_data.state==st]
+    states_data['case_increase']=(states_data.cases - states_data.cases.shift(1))
+    states_data['death_increase']=(states_data.deaths - states_data.deaths.shift(1))
+    print("retrieving states data")
+    return states_data
+
+stdata=get_states_data()
+
+
 app = Flask(__name__)
 
+
 @app.route('/get_usa_chart/')
-def create_chart(my_data=get_usa_data()): 
-    global graphJSON_usa_cases
-    trace_usa_cases = go.Scatter (
+def create_usa_chart(my_data=get_usa_data()): 
+    global graphJSON_usa_cases, graphJSON_usa_deaths
+    
+    trace_usa_cases = go.Bar (
         x = my_data.date,
         y = my_data.case_increase
         )
     data = [trace_usa_cases]
-    graphJSON_usa_cases = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)      
-    print("returning usa chart")
-    return render_template('index.html',  graphJSON_usa_cases=graphJSON_usa_cases)
+    
+    graphJSON_usa_cases = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder) 
+    
+    trace_usa_deaths = go.Bar (
+        x = my_data.date,
+        y = my_data.death_increase
+        )
+    data = [trace_usa_deaths]
+    graphJSON_usa_deaths = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder) 
+     
+    print("generating usa chart")
+    return render_template('index.html',  graphJSON_states_cases=graphJSON_states_cases, graphJSON_states_deaths=graphJSON_states_deaths, graphJSON_usa_cases=graphJSON_usa_cases, graphJSON_usa_deaths=graphJSON_usa_deaths)
+
+
+@app.route('/get_states_chart/')
+def create_states_chart(my_data=get_states_data()): 
+    global graphJSON_states_cases, graphJSON_states_deaths
+    
+    trace_states_cases = go.Bar (
+        x = my_data.date,
+        y = my_data.case_increase
+        )
+    data = [trace_states_cases]
+    
+    graphJSON_states_cases = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder) 
+    
+    trace_states_deaths = go.Bar (
+        x = my_data.date,
+        y = my_data.death_increase
+        )
+    data = [trace_states_deaths]
+    graphJSON_states_deaths = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder) 
+     
+    print("generating states chart")
+    return render_template('index.html',  graphJSON_states_cases=graphJSON_states_cases, graphJSON_states_deaths=graphJSON_states_deaths, graphJSON_usa_cases=graphJSON_usa_cases, graphJSON_usa_deaths=graphJSON_usa_deaths)
+
+
 
 @app.route('/SomeFunction')
 def SomeFunction():
