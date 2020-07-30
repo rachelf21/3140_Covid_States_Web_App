d1fec4f4c7b30b5b2745282ae8251312d1ca7140
Rachel Friedman <rachelf21@gmail.com>
2020-07-29 22:35:44 -0400

    added index.html and app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, url_for, jsonify, request
+import json
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import plotly 
+import plotly.graph_objs as go
+
+def get_usa_data():
+    usa_data = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv", error_bad_lines=False)
+    usa_data['date'] = pd.to_datetime(usa_data['date'], format="%Y-%m-%d" )
+    usa_data['date'] = usa_data['date'].dt.date
+    usa_data['case_increase']=(usa_data.cases - usa_data.cases.shift(1))
+    usa_data['death_increase']=(usa_data.deaths - usa_data.deaths.shift(1))
+    print("retrieving USA data")
+    return usa_data
+
+app = Flask(__name__)
+
+@app.route('/get_usa_chart/')
+def create_chart(my_data=get_usa_data()): 
+    trace_usa_cases = go.Scatter (
+        x = my_data.date,
+        y = my_data.case_increase
+        )
+    data = [trace_usa_cases]
+    graphJSON_usa_cases = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)      
+    print("returning usa chart")
+    return render_template('index.html',  graphJSON_usa_cases=graphJSON_usa_cases)
+
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+
+if __name__ == '__main__':
+  app.run(debug=False)
---
+++ static/js/scripts.js
@@ -0,0 +1,25 @@
+function msg() {
+  alert("Hello world! It works!");
+}
+
+/*
+function chart(){
+    var graph_usa_cases = {{graphJSON_usa_cases | safe}};
+var layout = {
+      title: 'Cases in the United States',
+          xaxis: {
+title: 'Date',
+showgrid: false,
+zeroline: false
+},
+yaxis: {
+title: 'Cases',
+showline: false
+}
+};
+Plotly.newPlot('usa',graph_usa_cases,layout);
+Plotly.newPlot('usa2',graph_usa_cases,layout);
+
+
+}
+*/
